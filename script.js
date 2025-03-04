c985d540ee640de58211a2dd793d12df2aeeed81
Knox Wong <wongknox@gmail.com>
2025-03-03 20:00:49 -0600

    Add modified js file

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ const num2 = 80 / 2; //second number in combo equals 40
 const num3 = 40 - 1; //third number in combo equals 39
 
 alert(`${message}: ${num1}-${num2}-${num3}`);
-//alert will incorporate the "string" and the const num1, num2, and numb3 to spell out the entire message with the vault combination
+//alert will incorporate the "string" and num1, num2, and num3 to spell out the entire message with the vault combination
