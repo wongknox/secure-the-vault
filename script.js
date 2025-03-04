cd5c26127417bf17377f88c3d1189d46885aacac
Knox Wong <wongknox@gmail.com>
2025-03-03 19:53:46 -0600

    Add  initial html,css,js files

---
+++ script.js
@@ -0,0 +1,10 @@
+const message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination";
+//this will be the pop up message or "string"
+
+const num1 = 9 + 1; //first number in combo equals 10
+const num2 = 80 / 2; //second number in combo equals 40
+const num3 = 40 - 1; //third number in combo equals 39
+
+alert(`${message}: ${num1}-${num2}-${num3}`);
+//alert will incorporate the "string" and the const num1, num2, and numb3 to spell out the entire message with the vault combination
